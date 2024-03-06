f1b3c40a76c8f46e50f2eb0a9a3d863033cd996a
NekkoS <alexandredaineko@yandex.ru>
2024-03-06 14:08:38 +0200

    step2.1

--- Step2/acrxEntryPoint.cpp
+++ Step2/acrxEntryPoint.cpp
@@ -1,74 +1,159 @@
+// (C) Copyright 2002-2012 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
 
+//-----------------------------------------------------------------------------
+//----- acrxEntryPoint.cpp
+//-----------------------------------------------------------------------------
 #include "StdAfx.h"
 #include "resource.h"
+#include "aced.h"
+#include "dbsymtb.h"
 #include <tchar.h>
-#include <iostream>
 
 //-----------------------------------------------------------------------------
 #define szRDS _RXST("ADSK")
 
 //-----------------------------------------------------------------------------
 //----- ObjectARX EntryPoint
-class CStep2App : public AcRxArxApp {
+class CStep1App : public AcRxArxApp {
 
 public:
-	CStep2App() : AcRxArxApp() {}
+	CStep1App () : AcRxArxApp () {}
 
-	virtual AcRx::AppRetCode On_kInitAppMsg(void* pkt) {
+	virtual AcRx::AppRetCode On_kInitAppMsg (void *pkt) {
 		// TODO: Load dependencies here
 
 		// You *must* call On_kInitAppMsg here
-		AcRx::AppRetCode retCode = AcRxArxApp::On_kInitAppMsg(pkt);
-
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kInitAppMsg (pkt) ;
+		
 		// TODO: Add your initialization code here
-
-		return (retCode);
+		RegisterCustomCommands();
+		return (retCode) ;
 	}
 
-	virtual AcRx::AppRetCode On_kUnloadAppMsg(void* pkt) {
+	virtual AcRx::AppRetCode On_kUnloadAppMsg (void *pkt) {
 		// TODO: Add your code here
 
 		// You *must* call On_kUnloadAppMsg here
-		AcRx::AppRetCode retCode = AcRxArxApp::On_kUnloadAppMsg(pkt);
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kUnloadAppMsg (pkt) ;
 
 		// TODO: Unload dependencies here
 
-		return (retCode);
+		return (retCode) ;
 	}
 
-	virtual void RegisterServerComponents() {
+	virtual void RegisterServerComponents () {
+	}
+	
+	// The ACED_ARXCOMMAND_ENTRY_AUTO macro can be applied to any static member 
+	// function of the CStep1App class.
+	// The function should take no arguments and return nothing.
+	//
+	// NOTE: ACED_ARXCOMMAND_ENTRY_AUTO has overloads where you can provide resourceid and
+	// have arguments to define context and command mechanism.
+	
+	// ACED_ARXCOMMAND_ENTRY_AUTO(classname, group, globCmd, locCmd, cmdFlags, UIContext)
+	// ACED_ARXCOMMAND_ENTRYBYID_AUTO(classname, group, globCmd, locCmdId, cmdFlags, UIContext)
+	// only differs that it creates a localized name using a string in the resource file
+	//   locCmdId - resource ID for localized command
+
+	// Modal Command with localized name
+	// ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL)
+	static void ADSKMyGroupMyCommand () {
+		// Put your command code here
+
 	}
 
-	static void AsdkStep2_requestPoint() {
-		ads_point point;
-		if (acedGetPoint(NULL, _T("Select a point: "), point) == RTNORM) {
-			acutPrintf(_T("\nSelected point: (%.2f, %.2f, %.2f)"), point[0], point[1], point[2]);
+	static void HelloWorld() {
+		acutPrintf(_T("\nHello, World!"));
+	}
+
+	void RegisterHelloWorldCommand() {
+		acedRegCmds->addCommand(_T("MYGROUP"), _T("HELLOWORLD"), _T("HELLOWORLD"), ACRX_CMD_MODAL, HelloWorld);
+	}
+
+	void RegisterCustomCommands() {
+		RegisterHelloWorldCommand();
+	}
+
+	// Modal Command with pickfirst selection
+	// ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MyPickFirst, MyPickFirstLocal, ACRX_CMD_MODAL | ACRX_CMD_USEPICKSET)
+	static void ADSKMyGroupMyPickFirst () {
+		ads_name result ;
+		int iRet =acedSSGet (ACRX_T("_I"), NULL, NULL, NULL, result) ;
+		if ( iRet == RTNORM )
+		{
+			// There are selected entities
+			// Put your command using pickfirst set code here
+		}
+		else
+		{
+			// There are no selected entities
+			// Put your command code here
 		}
 	}
 
-	static void AsdkStep2_requestAngle() {
-		double angle;
-		if (acedGetAngle(NULL, _T("Enter an angle: "), &angle) == RTNORM) {
-			acutPrintf(_T("\nEntered angle: %.2f degrees"), angle);
-		}
+	// Application Session Command with localized name
+	// ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MySessionCmd, MySessionCmdLocal, ACRX_CMD_MODAL | ACRX_CMD_SESSION)
+	static void ADSKMyGroupMySessionCmd () {
+		// Put your command code here
 	}
 
-	static void AsdkStep2_selectEntity() {
-		ads_name entity;
-		ads_point point;
+	// The ACED_ADSFUNCTION_ENTRY_AUTO / ACED_ADSCOMMAND_ENTRY_AUTO macros can be applied to any static member 
+	// function of the CStep1App class.
+	// The function may or may not take arguments and have to return RTNORM, RTERROR, RTCAN, RTFAIL, RTREJ to AutoCAD, but use
+	// acedRetNil, acedRetT, acedRetVoid, acedRetInt, acedRetReal, acedRetStr, acedRetPoint, acedRetName, acedRetList, acedRetVal to return
+	// a value to the Lisp interpreter.
+	//
+	// NOTE: ACED_ADSFUNCTION_ENTRY_AUTO / ACED_ADSCOMMAND_ENTRY_AUTO has overloads where you can provide resourceid.
+	
+	//- ACED_ADSFUNCTION_ENTRY_AUTO(classname, name, regFunc) - this example
+	//- ACED_ADSSYMBOL_ENTRYBYID_AUTO(classname, name, nameId, regFunc) - only differs that it creates a localized name using a string in the resource file
+	//- ACED_ADSCOMMAND_ENTRY_AUTO(classname, name, regFunc) - a Lisp command (prefix C:)
+	//- ACED_ADSCOMMAND_ENTRYBYID_AUTO(classname, name, nameId, regFunc) - only differs that it creates a localized name using a string in the resource file
 
-		int result = acedEntSel(_T("\nSelect entity"), entity, point);
+	// Lisp Function is similar to ARX Command but it creates a lisp 
+	// callable function. Many return types are supported not just string
+	// or integer.
+	// ACED_ADSFUNCTION_ENTRY_AUTO(CStep1App, MyLispFunction, false)
+	static int ads_MyLispFunction () {
+		//struct resbuf *args =acedGetArgs () ;
+		
+		// Put your command code here
 
-		if (result != RTNORM)
-			return;
+		//acutRelRb (args) ;
+		
+		// Return a value to the AutoCAD Lisp Interpreter
+		// acedRetNil, acedRetT, acedRetVoid, acedRetInt, acedRetReal, acedRetStr, acedRetPoint, acedRetName, acedRetList, acedRetVal
 
-		acutPrintf(_T("\nYou selected entity ID: %ld, %ld"), entity[0]);
+		return (RTNORM) ;
 	}
-};
+	
+} ;
 
 //-----------------------------------------------------------------------------
-IMPLEMENT_ARX_ENTRYPOINT(CStep2App)
+IMPLEMENT_ARX_ENTRYPOINT(CStep1App)
 
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep2App, AsdkStep2, _requestPoint, requestPoint, ACRX_CMD_TRANSPARENT, NULL);
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep2App, AsdkStep2, _requestAngle, requestAngle, ACRX_CMD_TRANSPARENT, NULL);
-ACED_ARXCOMMAND_ENTRY_AUTO(CStep2App, AsdkStep2, _selectEntity, selectEntity, ACRX_CMD_TRANSPARENT, NULL);
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL, NULL)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MyPickFirst, MyPickFirstLocal, ACRX_CMD_MODAL | ACRX_CMD_USEPICKSET, NULL)
+ACED_ARXCOMMAND_ENTRY_AUTO(CStep1App, ADSKMyGroup, MySessionCmd, MySessionCmdLocal, ACRX_CMD_MODAL | ACRX_CMD_SESSION, NULL)
+ACED_ADSSYMBOL_ENTRY_AUTO(CStep1App, MyLispFunction, false)
+
